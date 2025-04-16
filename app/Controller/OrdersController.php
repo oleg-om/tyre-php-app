83c6bee0c21769473d5fc43bb33c143335f5d463
Oleg Omelchenko <oleg.omelc93@gmail.com>
2025-04-16 17:07:35 +0300

    test

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -392,7 +392,7 @@ class OrdersController extends AppController {
                     $escapedJson = escapeshellarg($json);
 
                     // Составляем curl-запрос
-                    $cmd = "curl -k -X POST -H 'Content-Type: application/json' -d $escapedJson $$url";
+                    $cmd = "curl -v -k -X POST -H 'Content-Type: application/json' -d --noproxy '*' $escapedJson $$url";
 
                     // Выполняем
                    exec($cmd, $output, $ret);
