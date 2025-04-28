efb613d5beb8dec08a10eb66c9c0cad0ef73cf21
Oleg Omelchenko <oleg.omelc93@gmail.com>
2025-04-28 12:59:44 +0300

    supplier prefix

--- app/Model/Supplier.php
+++ app/Model/Supplier.php
@@ -1,24 +1,22 @@
 <?php
-class Supplier extends AppModel {
-	public $name = 'Supplier';
-	public $validationDomain = 'admin_suppliers';
-	public $validate = array(
-		'title' => array(
-			'rule' => 'notEmpty',
-			'message' => 'error_title_empty'
-		),
-        'delivery_time_from' => array(
-            array('required' => false)
+
+class Supplier extends AppModel
+{
+    public $name = 'Supplier';
+    public $validationDomain = 'admin_suppliers';
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty',
+            'message' => 'error_title_empty'
         ),
-        'delivery_time_to' => array(
-            array('required' => false)
-        ),
-        'prefix' => array(
-            array('required' => false)
-        )
-	);
-	public function __construct() {
-		parent::__construct();
-		$this->virtualFields['is_deletable'] = 1;
-	}
+        'delivery_time_from' => array(),
+        'delivery_time_to' => array(),
+        'prefix' => array()
+    );
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->virtualFields['is_deletable'] = 1;
+    }
 }
